2bb22d6fbd9e071b8fa48bc09c84f23217c2f1d1
bui quang huy <Codatduoc@gmail.com>
2020-07-05 15:05:35 +0700

    config send message

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const accountSid = 'ACd00286cd0a5b2ea7224b9fc9c06a9040';
-const authToken = '2e3e0faebbbde9ee77ba605d07d8be43';
+const authToken = '0c85196ae636885b129671025dd0029a';
 const client = require('twilio')(accountSid, authToken);
-
 var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
@@ -16,7 +15,6 @@ var connection = mysql.createConnection({
   port: '3307',
   database: 'restaurant'
 });
-
 connection.connect(function(err) {
   if (err) throw err;
   console.log('Connected!');
@@ -47,23 +45,22 @@ app.get('/dessert', (req, res) => {
 
   // connection.end();
 });
-app.get('/feedback', (req,res) =>{
+app.get('/feedback', (req, res) => {
   let sql = 'SELECT * FROM `feedback` ';
   connection.query(sql, (error, results, fields) => {
     if (error) throw error;
     res.end(JSON.stringify(results));
   });
-})
+});
 app.post('/check_booking', (req, res) => {
   // console.log(req.body);
   let sql = 'SELECT* FROM `booking` ';
   connection.query(sql, (err, results) => {
     if (err) console.log(err);
     const values = JSON.parse(JSON.stringify(results));
-    if (check.booking(req.body, values) === -2){
+    if (check.booking(req.body, values) === -2) {
       res.end('-2');
-    }
-   else if (check.booking(req.body, values) === 0) {
+    } else if (check.booking(req.body, values) === 0) {
       res.end('0');
 
       // res.end(`Xin lỗi ngày ${req.body.date}, vào thời điểm lúc ${req.body.time}h, bàn số ${req.body.table} đã có người đặt, quý khách vui lòng chọn vào khung giờ khác! `);
@@ -97,11 +94,9 @@ app.post('/booking', (req, res) => {
     '\nSố lượng người: ' +
     people +
     '\n\n====TRÂN TRỌNG====';
-  console.log(sms);
 
   // insert into database
   let sql = `INSERT INTO booking(name, phone, date,  time, people, number_table)  VALUES (?, ? , ?, ?, ?, ?)`;
-  console.log(sql);
 
   let values = [name, phone, date, time, people, number_table];
   connection.query(sql, values, (err, result, fields) => {
@@ -117,36 +112,42 @@ app.post('/booking', (req, res) => {
       from: '+12028049954',
       to: '+84973405092'
     })
-    .then(message => console.log(message.sid));
-  res.end('Đã gửi tin nhắn');
+    .then(message => {
+      console.log('Đã gửi tin nhắn');
+      console.log(message.sid);
+      res.end('Đã gửi tin nhắn');
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
-app.post('/check_feedback',(req, res) =>{
+app.post('/check_feedback', (req, res) => {
   let sql = 'SELECT* FROM `feedback` ';
   connection.query(sql, (err, results) => {
     if (err) console.log(err);
     const values = JSON.parse(JSON.stringify(results));
-    if (check.feedback(req.body, values) === -1){
+    if (check.feedback(req.body, values) === -1) {
       res.end('-1');
-    }
-     else {
+    } else {
       res.end('1');
     }
   });
-})
-app.post('/feedback', (req, res)=>{
-  console.log("FeedBack");
+});
+app.post('/feedback', (req, res) => {
+  console.log('FeedBack');
   console.log(req.body);
-  const {name, content, phone} = req.body
+  const { name, content, phone } = req.body;
   let sql = `INSERT INTO feedback(name, content,phone)  VALUES (?, ?, ? )`;
   console.log(sql);
-  let values = [name, content,phone];
+  let values = [name, content, phone];
   connection.query(sql, values, (err, result, fields) => {
     if (err) console.log(err);
     console.log(result);
   });
 
-  res.end('Ok')
-})
+  res.end('Ok');
+});
+
 const port = 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
